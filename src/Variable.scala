62204bba0d76561e6d3c04083f789759a0a6f2f6
Rahul Kumar <rahulcsofficial@gmail.com>
2021-06-13 14:35:58 +0530

    Diff berween variables and value
    Signed-off-by: Rahul Kumar <rahulcsofficial@gmail.com>

---
+++ src/Variable.scala
@@ -0,0 +1,26 @@
+object Variable extends App {
+
+println("Hello, Rahul")
+  var a = 100
+  a=101
+  println("variable "+a)
+
+  val b=100
+
+  println("value " + b)
+
+  var c:Int = 10
+  println("Int " + c)
+
+  var d:Float = 10.1F
+  println("Float " + d)
+
+  var e:Boolean = true
+  println("Boolean " + e)
+
+  var f:String = "Rahul"
+  println("String " + f)
+
+
+
+}
